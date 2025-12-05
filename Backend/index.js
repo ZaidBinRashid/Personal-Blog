ee4a17247364a57bbef6f111a51a53cb2eece71a
Zaid Bin Rashid <zaidbinrashid85@gmail.com>
2025-12-05 18:17:49 +0530

    Added the frontend folder and setted it up to  use react router

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser"
+import cors from "cors";
 import routes from "./routes/routes.js";
 
 dotenv.config();
@@ -10,6 +12,9 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({ origin:process.env.CORS_ORIGIN,credentials: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
 
 app.use("/api", routes);
 
